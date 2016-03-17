c1cd76608beb3ffd6e92f9eacc277595178027cd
YOUR NAME <blzhang.m@gmail.com>
2016-03-17 09:56:20 -0400

    modify

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 
 
+
 #include "matrix.h"
 #include "power.h"
 //#include "neumann.h"
@@ -8,8 +9,9 @@
 #include<stdio.h>
 #include "stdlib.h"
 #include "neumann.h"
-int main(){
-	freopen("in.txt", "r", stdin);
+int main(int argc, char *argv[]){
+	freopen("../in.txt", "r", stdin);
+	
 	int size = 3;
 	Matrix<double> A(size,size);
 	for(int i = 0; i < size; i++){
@@ -62,61 +64,4 @@ int main(){
 		cout << res1[i] << endl;
 	}
 	return 0;
-	
-
-/*	Matrix<double> A(3,3);
-	printmatrix(A);
-	for(int i = 1 ; i <= 3; i++){
-		vector<double> a;
-		for(int j = 1 ; j <=3; j++){
-			a.push_back(i* j);
-			A.setRow(i-1, a);
-		}
-	}
-	printmatrix(A);
-	Matrix<double> B(3,3);
-	for(int i = 1 ; i <= 3; i++){
-		vector<double> a;
-		for(int j = 1 ; j <=3; j++){
-			a.push_back(1);
-			B.setRow(i-1, a);
-		}
-	}
-	A = A *B;
-	printmatrix(A);
-	Matrix<double> D = A + B;
-	printmatrix(D);
-	Matrix<double> E = A - B;
-	printmatrix(E);
-	Matrix<double> F = A * 2;
-	printmatrix(F);
-	Matrix<double> G = A / 2.0;
-	printmatrix(G);
-	Matrix<double> H = 2.0/A;
-	printmatrix(H);
-
-	vector<double> a(3);
-	a[0] =1 ; a[1] = 1; a[2] = 1;
-
-	vector<double> I = A * a;
-	
-	cout << I[0] << "  " <<I[1] << "  " << I[2] << endl;
-	
-	vector<double> J = a *A;
-	
-	cout << J[0] << "  " <<J[1] << "  " << J[2] << endl;
-
-	cout <<"max  " << power(A) << endl;
-	Matrix<double> AA(2,2);
-	vector<double> aa(2);
-	aa[0] = 3; aa[1] = 1;
-	AA.setRow(0,aa);
-	vector<double> b(2);
-	b[0] = 3; b[1] = 1;
-	AA.setRow(1, b);
-	cout <<"max  " << power(AA) << endl;
-	return 0;
-	
-*/
-
 }
